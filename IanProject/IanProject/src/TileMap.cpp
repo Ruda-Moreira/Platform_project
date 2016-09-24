7bc55b1ff72e85dfa9c061fe417b789b949a4266
akitodr <lara.muller@workmail.com>
2016-09-24 17:55:59 -0300

    tile map faild

--- IanProject/IanProject/src/TileMap.cpp
+++ IanProject/IanProject/src/TileMap.cpp
@@ -3,33 +3,21 @@
 
 
 void TileMap::init() {
-	position = ofVec2f(500, 500);
-	tiles = new ofImage[5];
-	tiles[0].load("img/tile1.png");
-	tiles[1].load("img/tile2.png");
-	tiles[2].load("img/tile3.png");
-	tiles[3].load("img/tile4.png");
-	tiles[4].load("img/tile5.png");
+	position = ofVec2f(100, 100);
+	tiles = new ofImage[255];
+	tiles['!'].load("img/tile1.png");
+	tiles['@'].load("img/tile2.png");
+	tiles['*'].load("img/tile3.png");
+	tiles['$'].load("img/tile4.png");
+	tiles['#'].load("img/tile5.png");
 }
 
 void TileMap::draw() {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			if (map[i][j] == '!') {
-				tiles[0].draw(position);
-				//tiles[0].drawSubsection();
-			}
-			else if (map[i][j] == '@') {
-				tiles[1].draw(position);
-			}
-			else if (map[i][j] == '*') {
-				tiles[2].draw(position);
-			}
-			else if (map[i][j] == '$') {
-				tiles[3].draw(position);
-			}
-			else if (map[i][j] == '#') {
-				tiles[4].draw(position);
+			ofImage &tile = tiles[map[i][j]];
+			if (tile.isAllocated()) {
+				tile.draw(position.x + j* tile.getWidth(), position.y + i * tile.getHeight());
 			}
 		}
 	}
